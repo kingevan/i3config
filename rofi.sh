57e8c513cc52fa6b55872dd7b39eff1e4380fee5
kingevan <evan-007@hotmail.com>
2016-06-07 19:36:33 -0700

    Add files via upload

--- rofi.sh
+++ rofi.sh
@@ -1,2 +1,2 @@
 #This is a testing file for the rofi configuration
-rofi -show run -width 100 -lines 7 -columns 1 -fg "#554464" -bg "#111111" -hlfg "#86778A" -hlbg "#111111" -padding 10
+rofi -show run -width 100 -lines 7 -columns 1 -fg "#554464" -bg "#111111" -hlfg "#86778A" -hlbg "#111111" -padding 10 -opacity 90 
